003eba0272e8e79b9062e3eef3e6c6a205ad12da
VoidField101 <voidfield101@gmail.com>
2022-09-24 14:41:36 +0200

    Test implementation

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,16 @@
 // This software is released under the MIT License.
 // https://opensource.org/licenses/MIT
 
+use std::fs::File;
+
+use parse::parse_entry;
+
+mod format;
+mod error;
+mod parse;
+
+
 fn main() {
+    let mut file = File::open("test.ani").expect("Error");
+    parse_entry(&mut file).expect("Error");
 }
